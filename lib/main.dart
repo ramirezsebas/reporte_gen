0cd046e3ca6ac8155c3b4d433e3f2b45e8de31cf
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-02-16 00:25:46 -0300

    Config del main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,40 @@
-import 'package:flutter/material.dart';
-import 'package:report_generator/pages/home_page.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:floor/floor.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:report_generator/data/database/database.dart';
+import 'package:report_generator/data/providers/loading_provider.dart';
+import 'package:report_generator/data/providers/reporte_provider.dart';
+import 'package:report_generator/data/services/service_locator.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'package:report_generator/pages/reportes_page.dart';
+
+import 'pages/reportes_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory dataDirectory = await getApplicationDocumentsDirectory();
+  String dbPath = join(dataDirectory.path, "'app_database.db'");
+
+  print("La Ubicación de la Base de Datos en el telefono seria: $dbPath");
+
+  AppDatabase database =
+      await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+
+  setupServiceLocator(database);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ReporteProvider()),
+        ChangeNotifierProvider(create: (_) => LoadingProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,11 +44,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: ReportesPage(),
     );
   }
 }
-
