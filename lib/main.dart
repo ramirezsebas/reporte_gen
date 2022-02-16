119969ce7b550f58dc36c869a2047da05bd7e330
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-02-16 07:13:53 -0300

    Cambiamos el titulo

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Report Generator',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
