35938128cc17b6f891a29edbeb72e67381329e26
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-02-16 07:16:02 -0300

    Imports limpieza

--- lib/pages/reportes_page.dart
+++ lib/pages/reportes_page.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:report_generator/data/database/database.dart';
-import 'package:report_generator/data/models/reporte.dart';
-import 'package:report_generator/data/providers/loading_provider.dart';
-import 'package:report_generator/data/providers/reporte_provider.dart';
-import 'package:report_generator/data/services/service_locator.dart';
-import 'package:report_generator/pages/create_report_page.dart';
 import 'package:report_generator/shared/widgets/action_bar.dart';
-import 'package:report_generator/shared/widgets/reporte_card.dart';
 import 'package:report_generator/shared/widgets/reportes_stream_builder.dart';
 
 class ReportesPage extends StatelessWidget {
